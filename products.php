5f6650620800eed0257236ecff35a96902fb69b6
quanpro1 <leekwangsoofc1@gmail.com>
2023-03-23 20:10:43 +0700

    kiem tra login

--- products.php
+++ products.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-
+//kiem tra thong tin
+if(!isset($_SESSION['username'])){
+	header("Location: login.php");
+}
 $title = 'Product Page';
 include_once('layouts/header.php');
 require_once('db/dbhelper.php');
